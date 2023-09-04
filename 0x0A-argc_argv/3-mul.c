fda84789088a205d140dd913df218d1dcde95883
Luluseember <seemberior@gmail.com>
2023-09-04 12:53:27 -0700

    No arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,46 +5,50 @@
  * _atoi - converts a string to an integer
  * @s: string to be converted
  *
- * Return: the integer converted from the string
+ * Return: the int converted from the string
  */
 int _atoi(char *s)
 {
-	int i, d, n, f, a, figure;
+	int i, d, n, len, f, digit;
 
 	i = 0;
 	d = 0;
 	n = 0;
+	len = 0;
 	f = 0;
-	a = 0;
-	figure = 0;
+	digit = 0;
 
-	while (s[f] != '\0')
-		f++;
-	while (i < f && a == 0)
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
 			++d;
+
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			figure = s[i] - '0';
+			digit = s[i] - '0';
 			if (d % 2)
-				figure = -figure;
-			n = n * 10 + figure;
+				digit = -digit;
+			n = n * 10 + digit;
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
-			a = 0;
+			f = 0;
 		}
 		i++;
 	}
-	if (a == 0)
+
+	if (f == 0)
 		return (0);
+
 	return (n);
 }
 
 /**
  * main - multiplies two numbers
- * @argc: argumentscount
+ * @argc: number of arguments
  * @argv: array of arguments
  *
  * Return: 0 (Success), 1 (Error)
