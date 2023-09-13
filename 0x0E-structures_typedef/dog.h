841221efe0805a246bb08f4143111d870cf5fce2
Luluseember <seemberior@gmail.com>
2023-09-12 19:41:27 -0700

    simple

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,9 +17,9 @@ struct dog
 };
 
 /**
- * my_dog - typedef for struct dog
+ * dog_t my_dog - typedef for struct dog
  */
-typedef struct dog my_dog;
+typedef struct dog_t my_dog;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
